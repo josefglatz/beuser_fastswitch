ce9324980296bada33c1d39945cc1d7d572d3e94
Josef Glatz <jousch@gmail.com>
2019-08-06 07:11:54 +0200

    [TASK] Update .editorconfig
    based on actual TYPO3 core .editorconfig file.

--- Resources/Public/JavaScript/BeuserFastswitch.js
+++ Resources/Public/JavaScript/BeuserFastswitch.js
@@ -1,50 +1,50 @@
 define(['jquery', 'TYPO3/CMS/Backend/jquery.clearable'], function ($) {
-	var BeuserFastswitch = {};
+  var BeuserFastswitch = {};
 
-	BeuserFastswitch.init = function () {
-		$('#beuser-fastswitch-search-form').submit(function(e){
-			e.preventDefault();
-		});
-		$('#beuser-fastswitch-search-mask')
-			.clearable()
-			.on('keyup', BeuserFastswitch.delay(function (e) {
-				var search = $('#beuser-fastswitch-search-mask').val(),
-					ajaxUrl = TYPO3.settings.ajaxUrls['beuser_fastswitch_backend_userlookup'],
-					resultTag = $('#beuser-fastswitch-ajax-result');
+  BeuserFastswitch.init = function () {
+    $('#beuser-fastswitch-search-form').submit(function (e) {
+      e.preventDefault();
+    });
+    $('#beuser-fastswitch-search-mask')
+      .clearable()
+      .on('keyup', BeuserFastswitch.delay(function (e) {
+        var search = $('#beuser-fastswitch-search-mask').val(),
+          ajaxUrl = TYPO3.settings.ajaxUrls['beuser_fastswitch_backend_userlookup'],
+          resultTag = $('#beuser-fastswitch-ajax-result');
 
-				if (search.length >= 1 && e.keyCode !== 13) {
-					var ajaxUrlSearch = ajaxUrl + '&search=' + search;
+        if (search.length >= 1 && e.keyCode !== 13) {
+          var ajaxUrlSearch = ajaxUrl + '&search=' + search;
 
-					resultTag.html('');
+          resultTag.html('');
 
-					$.get(ajaxUrlSearch, {
-						format: 'html'
-					}).done(function (data) {
-						resultTag.html(data);
-					});
-				}
-				if (search.length === 0) {
-					$.get(ajaxUrl, {
-						format: 'html'
-					}).done(function (data) {
-						resultTag.html(data);
-					});
-				}
-			}, 250));
-	};
+          $.get(ajaxUrlSearch, {
+            format: 'html'
+          }).done(function (data) {
+            resultTag.html(data);
+          });
+        }
+        if (search.length === 0) {
+          $.get(ajaxUrl, {
+            format: 'html'
+          }).done(function (data) {
+            resultTag.html(data);
+          });
+        }
+      }, 250));
+  };
 
-	BeuserFastswitch.delay = function (callback, ms) {
-		var timer = 0;
-		return function() {
-			var context = this, args = arguments;
-			clearTimeout(timer);
-			timer = setTimeout(function () {
-				callback.apply(context, args);
-			}, ms || 0);
-		};
-	};
+  BeuserFastswitch.delay = function (callback, ms) {
+    var timer = 0;
+    return function () {
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        callback.apply(context, args);
+      }, ms || 0);
+    };
+  };
 
-	BeuserFastswitch.init();
+  BeuserFastswitch.init();
 
-	return BeuserFastswitch;
+  return BeuserFastswitch;
 });
