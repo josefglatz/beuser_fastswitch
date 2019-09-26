30f12e4ef01951f65c3846865bfbe4ca8c32c343
Georg Ringer <georgringer@users.noreply.github.com>
2019-09-26 04:24:48 +0200

    [TASK] Improve ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,37 +5,24 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'author' => 'Josef Glatz',
     'author_email' => 'josefglatz@gmail.com',
-    'author_company' => 'http://www.josefglatz.at',
+    'author_company' => 'https://www.josefglatz.at',
     'state' => 'stable',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
+    'clearCacheOnLoad' => true,
     'version' => '3.2.0',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'typo3' => '8.7.0-10.4.99',
-                    'php' => '7.2.0-0.0.0'
-                ],
-            'conflicts' =>
-                [
-                ],
-            'suggests' =>
-                [
-                ],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-10.4.99',
+            'php' => '7.2.0-0.0.0'
         ],
-    'autoload' =>
-        [
-            'psr-4' =>
-                [
-                    'JosefGlatz\\BeuserFastswitch\\' => 'Classes',
-                ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'JosefGlatz\\BeuserFastswitch\\' => 'Classes',
         ],
-    'autoload-dev' =>
-        [
-            'psr-4' =>
-                [
-                    'JosefGlatz\\BeuserFastswitch\\Tests\\' => 'Tests',
-                ],
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'JosefGlatz\\BeuserFastswitch\\Tests\\' => 'Tests',
         ],
+    ],
 ];
