74e592aee6d1a3185d76b473ee9be6b946cd1f86
Thomas Deuling <info@coding.ms>
2019-12-31 17:12:21 +0100

    [BUGFIX] Avoid displaying deleted users in toolbar widget

--- Classes/Domain/Repository/BackendUserRepository.php
+++ Classes/Domain/Repository/BackendUserRepository.php
@@ -25,6 +25,7 @@ class BackendUserRepository extends \TYPO3\CMS\Beuser\Domain\Repository\BackendU
         $queryBuilder->matching(
             $queryBuilder->logicalAnd([
                 $queryBuilder->equals('admin', 0),
+                $queryBuilder->equals('deleted', 0),
                 $queryBuilder->logicalOr([
                     $queryBuilder->like('username', "%$search%"),
                     $queryBuilder->like('realName', "%$search%"),
@@ -51,6 +52,7 @@ class BackendUserRepository extends \TYPO3\CMS\Beuser\Domain\Repository\BackendU
         $queryBuilder = $this->createQuery();
 
         $constraints[] = $queryBuilder->equals('admin', 0);
+        $constraints[] = $queryBuilder->equals('deleted', 0);
         if (!empty($uids)) {
             $constraints[] = $queryBuilder->in('uid', $uids);
         }
