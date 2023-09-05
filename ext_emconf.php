588970ea7d6d8ef797b9ed513441b43a0ae5cf4e
Josef Glatz <j.glatz@supseven.at>
2023-09-05 13:11:03 +0200

    [!!!][TASK] Raise version constraints
    Raise PHP version to 8.1 or higher
    
    Related: #38

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Fast backend user switch for TYPO3 CMS administrator users.',
     'category' => 'be',
     'author' => 'Josef Glatz',
-    'author_email' => 'josefglatz@gmail.com',
+    'author_email' => 'typo3@josefglatz.at',
     'author_company' => 'https://www.josefglatz.at',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'version' => '4.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.9.99',
-            'php' => '7.4.1'
+            'typo3' => '11.5.0-12.4.99',
+            'php' => '8.1.0'
         ],
     ],
     'autoload' => [
