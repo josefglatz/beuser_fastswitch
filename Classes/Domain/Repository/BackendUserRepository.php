167650e67c97e78cc4309756bfd4ab7e773b8140
Josef Glatz <j.glatz@supseven.at>
2023-09-05 13:35:43 +0200

    [!!!][TASK] Remove TYPO3 11 support
    Related: #42

--- Classes/Domain/Repository/BackendUserRepository.php
+++ Classes/Domain/Repository/BackendUserRepository.php
@@ -3,7 +3,6 @@
 namespace JosefGlatz\BeuserFastswitch\Domain\Repository;
 
 use TYPO3\CMS\Beuser\Domain\Model\BackendUser;
-use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
@@ -11,13 +10,6 @@ use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 class BackendUserRepository extends \TYPO3\CMS\Beuser\Domain\Repository\BackendUserRepository
 {
     protected $objectType = BackendUser::class;
-
-    protected $typo3Version;
-
-    public function __construct(Typo3Version $typo3Version)
-    {
-        $this->typo3Version = $typo3Version->getBranch();
-    }
 
     /**
      * @param string $search
@@ -30,33 +22,18 @@ class BackendUserRepository extends \TYPO3\CMS\Beuser\Domain\Repository\BackendU
 
         $queryBuilder = $this->createQuery();
 
-        if ($this->typo3Version === '12.0') {
-            $queryBuilder->matching(
-                $queryBuilder->logicalAnd(
-                    $queryBuilder->equals('admin', 0),
-                    $queryBuilder->equals('deleted', 0),
-                    $queryBuilder->logicalOr(
-                        $queryBuilder->like('username', "%$search%"),
-                        $queryBuilder->like('realName', "%$search%"),
-                        $queryBuilder->like('email', "%$search%"),
-                        $queryBuilder->equals('uid', (int)$search)
-                    )
+        $queryBuilder->matching(
+            $queryBuilder->logicalAnd(
+                $queryBuilder->equals('admin', 0),
+                $queryBuilder->equals('deleted', 0),
+                $queryBuilder->logicalOr(
+                    $queryBuilder->like('username', "%$search%"),
+                    $queryBuilder->like('realName', "%$search%"),
+                    $queryBuilder->like('email', "%$search%"),
+                    $queryBuilder->equals('uid', (int)$search)
                 )
-            );
-        } else {
-            $queryBuilder->matching(
-                $queryBuilder->logicalAnd([
-                    $queryBuilder->equals('admin', 0),
-                    $queryBuilder->equals('deleted', 0),
-                    $queryBuilder->logicalOr([
-                        $queryBuilder->like('username', "%$search%"),
-                        $queryBuilder->like('realName', "%$search%"),
-                        $queryBuilder->like('email', "%$search%"),
-                        $queryBuilder->equals('uid', (int)$search)
-                    ])
-                ])
-            );
-        }
+            )
+        );
 
         $queryBuilder->setOrderings([
             'lastlogin' => QueryInterface::ORDER_DESCENDING,
@@ -81,19 +58,11 @@ class BackendUserRepository extends \TYPO3\CMS\Beuser\Domain\Repository\BackendU
             $constraints[] = $queryBuilder->in('uid', $uids);
         }
 
-        // @todo Remove TYPO3v11 support: remove if
-        if ($this->typo3Version === '12.0') {
-            $queryBuilder->matching(
-                $queryBuilder->logicalAnd(
-                    ...$constraints
-                )
-            );
-        } else {
-            $queryBuilder->matching(
-                $queryBuilder->logicalAnd($constraints)
-            );
-        }
-
+        $queryBuilder->matching(
+            $queryBuilder->logicalAnd(
+                ...$constraints
+            )
+        );
 
         $queryBuilder->setOrderings([
             'lastlogin' => QueryInterface::ORDER_DESCENDING,
