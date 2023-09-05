9a7f1830cf7dc934ad22d39da90df85847b22e75
Josef Glatz <j.glatz@supseven.at>
2023-09-05 14:41:52 +0200

    [!!!][TASK] Temporary workaround: set request from $GLOBALS
    The toolbar item implementation needs a rewrite for TYPO3 12. Therefore
    the request is set from $GLOBALS['TYPO3_REQUEST'] as temporary solution.
    
    All f:translate scenarios were adopted to also use the extensionName as
            $view->getRequest()->setControllerExtensionName('BeuserFastswitch');
    can not be used anymore as there is no setControllerExtensionName method
    anymore.
    
    Related: #43

--- Classes/Hooks/Backend/Toolbar/BackendUserPreviewToolbarItem.php
+++ Classes/Hooks/Backend/Toolbar/BackendUserPreviewToolbarItem.php
@@ -156,7 +156,7 @@ class BackendUserPreviewToolbarItem implements ToolbarItemInterface
 
         $view->setTemplate($filename);
 
-        $view->getRequest()->setControllerExtensionName('BeuserFastswitch');
+        $view->setRequest($GLOBALS['TYPO3_REQUEST']);
         return $view;
     }
 
