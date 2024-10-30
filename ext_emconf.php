4dcbe503160223c18e0945daee0089d552103d0a
Josef Glatz <typo3@josefglatz.at>
2024-10-30 12:43:37 +0100

    [!!!][TASK] Raise version constraints for PHP and TYPO3 Core
    Old: PHP 8.1-8.2
    New: PHP 8.2-8.3
    
    Old: TYPO3 Core 12.4
    New: TYPO3 Core 13.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'https://www.josefglatz.at',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '5.0.3',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.5-12.4.99',
-            'php' => '8.1.0-8.2.99'
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.3.99'
         ],
     ],
     'autoload' => [
